2106b321980362b0d0909d9600860904148d1e33
Ryan Gunn <Ryan.Gunn@dma.co.za>
2019-08-14 09:08:15 +0200

    fix: Add section 8
    -Add .gitignore file

---
+++ Section-8/107-Using-props/src/components/User.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button v-on:click="changeName">Change my Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <!-- Hard code name being passed -->
+                <!-- <app-user-detail name="ryan"></app-user-detail> -->
+                <!-- Dynamic value -->
+                <app-user-detail v-bind:name="name"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+        data: function() {
+            return {
+                name: 'John'
+            }
+        },
+        methods: {
+            changeName() {
+                this.name = 'Smith'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
